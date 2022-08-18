f806b3b594f8c819478aec7ea92e25dd8cd96cb5
Marco Antoni <marco.antoni910@gmail.com>
2022-08-18 12:35:23 -0300

    Responsividade e uso do require

---
+++ menu.php
@@ -0,0 +1,15 @@
+<header>
+	<nav>
+		<a class="logo" href="/">Tiger codes</a>
+		<div class="mobile-menu">
+			<div class="line1"></div>
+			<div class="line2"></div>
+			<div class="line3"></div>
+		</div>
+		<ul class="nav-list">
+			<li><a href="mostrar_agenda.php">Início</a></li>
+			<li><a href="mostrar_agenda.php">Gerenciar contatos</a></li>
+			<li><a href="mostrar_grupos.php">Gerenciar grupos</a></li>
+		</ul>
+	</nav>
+</header>
